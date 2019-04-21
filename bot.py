f7c484ef2db38c212c0edb1b26bc7a2dae4a1a63
Arjoonn Sharma <arjoonn.94@gmail.com>
2019-04-21 18:31:31 +0530

    ignore only 8 lines of code

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from telegram.ext import Updater, MessageHandler, Filters
 parser = argparse.ArgumentParser()
 parser.add_argument('token', help='Bot token')
 parser.add_argument('--shelf', default='shelf', help='Where to store shelf data')
-parser.add_argument('-n_code_lines_ok', default=10, help='How many lines of code like stuff is acceptable?')
+parser.add_argument('-n_code_lines_ok', default=8, help='How many lines of code like stuff is acceptable?')
 parser.add_argument('-ignore_n_offences', default=3, help='How many offences to ignore before reminding a person?')
 args = parser.parse_args()
 
